abcb4e51b2fbe83de95e9822a637a55214e38e52
bahati <bulozealfani@gmail.com>
2023-03-31 00:17:18 -0500

    creating a create method

--- backend.py
+++ backend.py
@@ -1,5 +1,17 @@
 import openai
 
-
+# create a new API key in OpenAI and paste it here
 class Chatbot:
-    pass
+    def __int__(self):
+        # create a new API key in OpenAI and paste it here
+        openai.api_key ="your_key"
+
+    def get_response(self, user_input):
+        responds = openai.Completion.create(
+            egine = "text-davinci-003",
+            prompt = user_input,
+            # tokens are words
+            max_tokens=4000,
+            # low temperature is more accurate answers but less diverse, more temperature is the opposite
+            temperature=0.5
+        )
